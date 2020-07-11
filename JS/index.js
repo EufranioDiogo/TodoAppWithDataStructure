2ff61be96825a19a84623c6fdeb59abdb385f97b
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-07-11 10:55:28 +0100

    Pushing the repository to Github

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,6 @@
 let addToDo = document.querySelector('#buttonAdd')
+let modalPopup = document.querySelector('.modalConteiner')
+
 
 class ToDoItem {
     constructor(titulo, prioridade) {
@@ -27,6 +29,7 @@ class ToDoList {
         this.tail = null;
         this.size = 0;
     }
+
     addToDo(titulo, prioridade) {
         if (prioridade > 0 && prioridade < 5) {
             let newItem = new ToDoItem(titulo, Number(prioridade));
@@ -34,6 +37,7 @@ class ToDoList {
             if (this.size == 0) {
                 this.head = newItem;
                 this.tail = newItem;
+                document.getElementById('buttonLimpar1').style.display = 'block';
             } else if (this.tail.getPrioridade() >= prioridade) {
                 newItem.prev = this.tail;
                 this.tail.next = newItem;
@@ -63,19 +67,30 @@ class ToDoList {
         }
     }
 
+    reset() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        document.getElementById('buttonLimpar1').style.display = 'none';
+        renderToDoItens(this)
+    }
+
     completeToDo(toDoPosition) {
         if (toDoPosition > 0 && toDoPosition <= this.size) {
             let result = null;
+
+            document.getElementById('buttonLimpar2').style.display = 'block';
 
             if (toDoPosition == 1 && this.size == 1) {
                 result = this.head;
                 this.head = null;
                 this.tail = null;
-            }else if(toDoPosition == 1){
+                document.getElementById('buttonLimpar1').style.display = 'none';
+            } else if (toDoPosition == 1) {
                 result = this.head;
                 this.head = this.head.next;
                 this.head.prev = null;
-            }else if (toDoPosition == this.size) {
+            } else if (toDoPosition == this.size) {
                 result = this.tail;
                 this.tail = this.tail.prev;
                 this.tail.next = null;
@@ -94,31 +109,73 @@ class ToDoList {
         }
     }
 
-    printData() {
-        let aux = this.head;
-        while (aux != null) {
-            console.log(aux.getTitle() + ' - ' + aux.getPrioridade());
-            aux = aux.getNext();
+    editToDo(toDoPosition, newTitle, newPrioridade) {
+        if (toDoPosition && toDoPosition <= this.size) {
+            this.removeToDo(toDoPosition)
+            this.addToDo(newTitle, newPrioridade)
         }
+    }
+    removeToDo(toDoPosition) {
+        if (toDoPosition > 0 && toDoPosition <= this.size) {
+            let result = null;
+            if (toDoPosition == 1 && this.size == 1) {
+                this.head = null;
+                this.tail = null;
+                document.getElementById('buttonLimpar1').style.display = 'none';
+            } else if (toDoPosition == 1) {
+                this.head = this.head.next;
+                this.head.prev = null;
+            } else if (toDoPosition == this.size) {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+            } else {
+                result = this.head;
+
+                for (let i = 0; i < toDoPosition - 1; i++) {
+                    result = result.next;
+                }
+
+                result.prev.next = result.next;
+                result.next.prev = result.prev;
+            }
+            this.size -= 1;
+            renderToDoItens(this)
+        }
+    }
+    getToDo(toDoPosition) {
+        if (toDoPosition > 0 && toDoPosition <= this.getSize()) {
+            let result = this.head;
+
+            for (let i = 0; i < toDoPosition - 1; i++) {
+                result = result.next;
+            }
+            return result
+        }
+    }
+    getSize() {
+        return this.size;
     }
 }
 
 
-function addTodoInCompletedList(todo){
+function addTodoInCompletedList(todo) {
     completedToDoList.push(todo);
     renderCompletedItens();
 }
 
-function cleanConteiner(conteiner){
+function cleanConteiner(conteiner) {
     conteiner.innerHTML = '';
 }
 
-function renderToDoItens(todoList){
+function renderToDoItens(todoList) {
     let conteinerToDo = document.querySelector('#conteinerToDo');
 
     cleanConteiner(conteinerToDo);
     let id = 1;
     let divToDoItem;
+    let divPainelDeControle;
+    let editButton;
+    let deleteButton;
     let i;
     let span;
     let p;
@@ -126,7 +183,7 @@ function renderToDoItens(todoList){
 
 
     let aux = todoList.head;
-    while(aux != null){
+    while (aux != null) {
         divToDoItem = document.createElement('div');
 
         divToDoItem.setAttribute('class', 'toDoItem')
@@ -145,6 +202,22 @@ function renderToDoItens(todoList){
         span.appendChild(p)
         divToDoItem.appendChild(span)
 
+
+        divPainelDeControle = document.createElement('div')
+        divPainelDeControle.setAttribute('class', 'optionToDo')
+
+        divPainelDeControle.setAttribute('style', 'display:none')
+        editButton = document.createElement('i')
+        deleteButton = document.createElement('i')
+
+        editButton.setAttribute('class', 'fas fa-edit edit')
+        deleteButton.setAttribute('class', 'fas fa-trash-alt trash')
+
+        divPainelDeControle.appendChild(editButton)
+        divPainelDeControle.appendChild(deleteButton)
+
+        divToDoItem.appendChild(divPainelDeControle)
+
         nivelPrioridade = document.createElement('div')
         nivelPrioridade.setAttribute('class', 'nivelPrioridade prioridade' + aux.getPrioridade())
         divToDoItem.appendChild(nivelPrioridade)
@@ -157,20 +230,54 @@ function renderToDoItens(todoList){
     addTheFunctionToCompleteTask(document.querySelector('#conteinerToDo'))
 }
 
-function addTheFunctionToCompleteTask(conteiner){
-    for(let i = 0; i < conteiner.childElementCount; i++){
+function addTheFunctionToCompleteTask(conteiner) {
+    for (let i = 0; i < conteiner.childElementCount; i++) {
         conteiner.childNodes[i].childNodes[0].onclick = () => {
             todoList.completeToDo(conteiner.childNodes[i].getAttribute('id'))
+        }
+        conteiner.childNodes[i].childNodes[2].childNodes[1].onclick = () => {
+            todoList.removeToDo(conteiner.childNodes[i].getAttribute('id'))
+        }
+        conteiner.childNodes[i].childNodes[2].childNodes[0].onclick = () => {
+            let node = todoList.getToDo(conteiner.childNodes[i].getAttribute('id'))
+    
+            flagVisible = 1;
+
+            modalPopup.style.display = 'block';
+            document.getElementById('inputTarefa').value = node.getTitle()
+            document.getElementById('inputPrioridade').value = node.getPrioridade()
+
+            document.querySelector('#buttonAdd').textContent = 'Atualizar Tarefa'
+
+            document.querySelector('#buttonAdd').onclick = () => {
+                if (! (document.getElementById('inputTarefa').value == node.getTitle() && document.getElementById('inputPrioridade').value == node.getPrioridade())){
+                    if(document.getElementById('inputTarefa').value != node.getTitle() && document.getElementById('inputPrioridade').value == node.getPrioridade()){
+                        node.title = document.getElementById('inputTarefa').value;
+                        renderToDoItens()
+                    } else {
+                        newTitle = document.getElementById('inputTarefa').value;
+                        newPrioridade = document.getElementById('inputPrioridade').value
+                
+                        todoList.editToDo(conteiner.childNodes[i].getAttribute('id'), newTitle, newPrioridade)
+                    }
+                }
+            }
+        }
+        conteiner.childNodes[i].childNodes[1].onclick = () => {
+            if (conteiner.childNodes[i].childNodes[2].getAttribute('style').split(':')[1] == 'none') {
+                conteiner.childNodes[i].childNodes[2].setAttribute('style', 'display:block')
+            } else {
+                conteiner.childNodes[i].childNodes[2].setAttribute('style', 'display:none')
+            }
         }
     }
 }
 
-function completeItem(id){
+function completeItem(id) {
     todoList.completeToDo(id);
 }
 
-
-function renderCompletedItens(){
+function renderCompletedItens() {
     let conteinerToDo = document.querySelector('#conteinerDone');
 
     cleanConteiner(conteinerToDo);
@@ -182,7 +289,7 @@ function renderCompletedItens(){
 
 
     let aux = 0;
-    while(aux < completedToDoList.length){
+    while (aux < completedToDoList.length) {
         divToDoItem = document.createElement('div');
 
         divToDoItem.setAttribute('class', 'toDoItem')
@@ -218,14 +325,39 @@ function renderCompletedItens(){
 let completedToDoList = []
 let todoList = new ToDoList();
 
-addToDo.onclick = function (){
+addToDo.onclick = function () {
     let textoToDo = document.getElementById('inputTarefa').value
     let prioridade = document.getElementById('inputPrioridade').value
 
-    if(textoToDo.trim().length > 0 && prioridade > 0 && prioridade < 5){
+    if (textoToDo.trim().length > 0 && prioridade > 0 && prioridade < 5) {
         todoList.addToDo(textoToDo, Number(prioridade))
         document.getElementById('inputTarefa').value = ""
-    }else{
+    } else {
         alert('Dados inválidos')
+    }
+}
+
+document.getElementById('buttonLimpar1').onclick = () => {
+    if (todoList.getSize() > 0) {
+        todoList.reset();
+    }
+}
+
+document.getElementById('buttonLimpar2').onclick = () => {
+    if (completedToDoList.length > 0) {
+        completedToDoList = []
+        document.getElementById('buttonLimpar2').style.display = 'none';
+        renderCompletedItens()
+    }
+}
+
+let buttonClosePopup = document.querySelector('#buttonClose')
+
+buttonClosePopup.onclick = () => {
+    if (flagVisible) {
+        modalPopup.style.display = 'none';
+        document.getElementById('inputTarefa').value = '';
+        getElementById('inputPrioridade').value = '4';
+        flagVisible = 0;
     }
 }
--- JS/openAndClosePopup.js
+++ JS/openAndClosePopup.js
@@ -1,20 +1,20 @@
 let flagVisible = 0
 
 let addToDoButton = document.querySelector('#addNewToDo')
-let modalPopup = document.querySelector('.modalConteiner')
-
-let buttonClosePopup = document.querySelector('#buttonClose')
 
 addToDoButton.onclick = () => {
     if (!flagVisible){
         modalPopup.style.display = 'block';
         flagVisible = 1;
+        document.querySelector('#buttonAdd').textContent = 'Adicionar Tarefa'
     }
 }
 
 buttonClosePopup.onclick = () => {
     if (flagVisible){
         modalPopup.style.display = 'none';
+        document.getElementById('inputTarefa').value = '';
+        document.getElementById('inputPrioridade').value = '4';
         flagVisible = 0;
     }
 }
